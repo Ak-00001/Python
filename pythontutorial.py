931f55577528d5bff73b7b8f100a02e10c000c7c
Ak-00001 <ayushkshatri05@gmail.com>
2026-05-25 23:57:48 +0200

    first

--- pythontutorial.py
+++ pythontutorial.py
@@ -1,6 +1,14 @@
 #Python
 print("Hello")
+
+#String
 name="Bro"
 print(f"hello{name}")
 fav_food="Pizza"
 print(f"Your favourite food is {fav_food}")
+
+#Integer
+age=25
+print(f"you are {age} years old")
+
+price
