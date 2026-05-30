b37b8a2a62bb2693af7c6938f2e28b07d16bf71c
Ak-00001 <ayushkshatri05@gmail.com>
2026-05-30 20:49:35 +0200

    Format Specifier

---
+++ python9.py
@@ -0,0 +1,11 @@
+#Format Specifiers = {value : flags} format a value based on what flags are inserted
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+print(f"price 1 is {price1:<10}")
+print(f"price 2 is {price2:^10}")
+print(f"price 3 is {price3:.2f}")
+
+ 
