a8f9f6a80df2753f71b3b5df8e2ebc5054c8102d
Ak-00001 <ayushkshatri05@gmail.com>
2026-06-02 23:43:07 +0200

    For loops

---
+++ python11.py
@@ -0,0 +1,13 @@
+#for loops
+ 
+for x in range(1,11):
+    print(x)
+
+for x in reversed(range(1,11)):
+    print(x)
+
+for x in range(1,21):
+    if x==12:
+        break
+    else:
+        print(x)
