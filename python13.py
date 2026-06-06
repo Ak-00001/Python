c08697e74c039e6ff695d9feb4f0e3ed933e4356
Ak-00001 <ayushkshatri05@gmail.com>
2026-06-06 23:40:48 +0200

    Dictionary

--- python13.py
+++ python13.py
@@ -19,3 +19,12 @@ print("pineapple" in fruits)
 fruits.add('pineapple')
 fruits.remove("apple")
 fruits.pop()
+
+fruits = ("apple", "orange","banana","coconut","coconut")
+print(dir(fruits))
+print(help(fruits))
+print(len(fruits))
+print("pineapple" in fruits)
+
+#print(fruits.index("apple"))
+print(fruits.count("coconut"))
