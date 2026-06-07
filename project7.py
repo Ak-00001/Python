6dd0d85a7653301ae1dc18e6486544e12c070e78
Ak-00001 <ayushkshatri05@gmail.com>
2026-06-07 23:38:37 +0200

    Shopping cart program

---
+++ project7.py
@@ -0,0 +1,14 @@
+#Shopping cart program
+
+foods = ()
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit)")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}"))
+        foods.append(food)
+        prices.append(price)
