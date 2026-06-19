379462cddfdc25f2e60845068c235949f81a12ab
Ak-00001 <ayushkshatri05@gmail.com>
2026-06-19 23:15:05 +0200

    pr2

---
+++ practice2.py
@@ -0,0 +1,17 @@
+# Python Calculator
+
+print("Calculator")
+operator = input("Enter the operator (+,-,/,*)")
+op1 = int(input("Enter operand 1"))
+op2 = int(input("Enter operand 2"))
+if operator == "+":
+    print(f"The Addition of {op1} and {op2} is {op1+op2}")
+elif operator == "-":
+    print(f"The Subtraction of {op1} and {op2} is {op1-op2}")
+elif operator == "/":
+    print(f"The Division of {op1} and {op2} is {op1/op2}")
+elif operator == "*":
+    print(f"The Multliplication of {op1} and {op2} is {op1*op2}")
+else:
+    print("Invalid operation")
+
