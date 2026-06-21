5a4151d90b0dcf0214fcc943b7f891e08b93a248
Ak-00001 <ayushkshatri05@gmail.com>
2026-06-21 23:25:27 +0200

    mod

--- practice2.py
+++ practice2.py
@@ -1,7 +1,7 @@
-# Python Calculator
+# Python Calculator0
 
 print("Calculator")
-operator = input("Enter the operator (+,-,/,*)")
+operator = input("Enter the operator (+,-,/,*,%)")
 op1 = int(input("Enter operand 1"))
 op2 = int(input("Enter operand 2"))
 if operator == "+":
@@ -12,6 +12,8 @@ elif operator == "/":
     print(f"The Division of {op1} and {op2} is {op1/op2}")
 elif operator == "*":
     print(f"The Multliplication of {op1} and {op2} is {op1*op2}")
+elif operator == "%":
+    print(f"The Modulus of {op1} and {op2} is {op1%op2}")
 else:
     print("Invalid")
 
